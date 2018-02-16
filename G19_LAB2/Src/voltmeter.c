79c6205b01adfdac54aae1b8203a7f0899263894
DestructiveReasoning <harleyw@hotmail.com>
2018-02-15 22:57:23 -0500

    10s rms, min, max

--- G19_LAB2/Src/voltmeter.c
+++ G19_LAB2/Src/voltmeter.c
@@ -37,38 +37,59 @@ void plot_point(float input, float* output) {
 	static int count = 0;
 	static float min_val = 10.0;
 	static float max_val = 0.0;
-	static float rms_counter = 0.0;
-	static float min_queue[10];
-	static float max_queue[10];
-	static int seconds_counter = 0;
-	static int queue_length = 0;
+	static float rms_counter = -1.0;
+	static float min_queue[5];
+	static float max_queue[5];
+	static float rms_queue[10];
+	static int m_seconds_counter = 0;
+	static int m_queue_length = 0;
+	static int r_seconds_counter = 0;
+	static int r_queue_length = 0;
 
-	if(count == 0) {
-		rms_counter = input * input;
+	if(min_val > 4.0) {
 		min_val = input;
 		max_val = input;
-		min_queue[seconds_counter] = min_val;
-		max_queue[seconds_counter] = max_val;
-		seconds_counter = (seconds_counter + 1) % 10;
-		queue_length = (queue_length >= 10) ? 10 : queue_length + 1;
+		rms_counter = input * input;
+	}
+
+	if(count == 0) {
+		min_queue[m_seconds_counter] = min_val;
+		max_queue[m_seconds_counter] = max_val;
+		min_val = input;
+		max_val = input;
+		m_seconds_counter = (m_seconds_counter + 1) % 5;
+		m_queue_length = (m_queue_length >= 5) ? 5 : m_queue_length + 1;
+		rms_queue[r_seconds_counter] = rms_counter;
+		r_seconds_counter = (r_seconds_counter + 1) % 10;
+		r_queue_length = (r_queue_length >= 10) ? 10 : r_queue_length + 1;
+		rms_counter = input * input;
 	} 
 	else {
-		rms_counter += input * input;
+		if(count % 50 == 0) {
+			rms_queue[r_seconds_counter] = rms_counter;
+			r_seconds_counter = (r_seconds_counter + 1) % 10;
+			r_queue_length = (r_queue_length >= 10) ? 10 : r_queue_length + 1;
+			rms_counter = input * input;
+		} else rms_counter += input * input;
 		if(input < min_val) min_val = input;
 		else if(input > max_val) max_val = input;
 	}
 
-	output[RMS_MODE] = sqrt(rms_counter / (count + 1));
+	float total_squares = 0.0;
 	int c;
+	for(c = 0; c < r_queue_length; c++) {
+		total_squares += rms_queue[c];
+	}
+	output[RMS_MODE] = sqrt(total_squares / (r_queue_length * 50));
 	float m = min_queue[0];
-	for(c = 1; c < queue_length; c++) {
+	for(c = 1; c < m_queue_length; c++) {
 		if(min_queue[c] < m) m = min_queue[c];
 	}
 	output[MIN_MODE] = m;
 	m = max_queue[0];
-	for(c = 1; c < queue_length; c++) {
+	for(c = 1; c < m_queue_length; c++) {
 		if(max_queue[c] > m) m = max_queue[c];
 	}
 	output[MAX_MODE] = m;
-	count = (count + 1) % 50;
+	count = (count + 1) % 100;
 }
